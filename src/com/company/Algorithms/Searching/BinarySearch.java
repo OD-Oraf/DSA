277aff648e2cf873b8ffedcd159041b807d4bb72
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-08-31 22:20:26 -0400

    implemented binary search

--- src/com/company/Algorithms/Searching/BinarySearch.java
+++ src/com/company/Algorithms/Searching/BinarySearch.java
@@ -2,10 +2,29 @@ package com.company.Algorithms.Searching;
 
 public class BinarySearch {
 
-
+    static int binarySearch(int[] arr, int target){
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right){
+            //will fail with large values of left, right
+//            int mid = (left + right) / 2;
+            int mid = left + (right - left)/2;
+            if (target < arr[mid]){
+                right = mid - 1;
+            } else if (target > arr[mid]){
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args){
-        int arr[] = { 2, 3, 4, 10, 40 };
+        int[] arr = { 3, 4, 5, 6, 7, 9, 21,56 };
+        int target = 4;
+        System.out.println("Target found at index " + binarySearch(arr, target));
+
     }
 
 }
