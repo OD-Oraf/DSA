1d617b826127afe5ee702331f99f230f95d62632
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-09-13 00:11:03 -0400

    hashmap using generics

--- src/com/company/collections/MapInterface.java
+++ src/com/company/collections/MapInterface.java
@@ -7,20 +7,18 @@ import java.util.Set;
 
 public class MapInterface {
     public static void main(String[] args){
-        Map map = new HashMap();
+        HashMap<Integer, String> map = new HashMap<>();
         map.put(1, "Andy");
         map.put(5, "Randy");
         map.put(2, "Jack");
         map.put(6, "Armin");
 
         //Traverse map
-        Set set = map.entrySet();
-        Iterator itr = set.iterator();
-
-        while (itr.hasNext()){
-            Map.Entry entry = (Map.Entry)itr.next();
-            System.out.println(entry.getKey() + " " + entry.getValue());
+        for (Map.Entry m : map.entrySet()){
+            System.out.println(m.getKey() + " " + m.getValue());
         }
+
+
 
     }
 }
