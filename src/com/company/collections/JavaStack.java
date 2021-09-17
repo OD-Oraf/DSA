eb34d2bab9cc53e45e8d7a085fbb3a48ad86fbf7
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-09-17 12:37:25 -0400

    java stack

---
+++ src/com/company/collections/JavaStack.java
@@ -0,0 +1,25 @@
+package com.company.collections;
+
+import java.util.Stack;
+
+public class JavaStack {
+    public static void main(String[] args){
+        Stack<Integer> stk = new Stack<>();
+        //Stack empty() method
+        boolean result = stk.empty();
+
+        System.out.println("Is the stack empoty? " + result);
+        //Stack push() method
+        stk.push(78);
+        stk.push(113);
+        stk.push(90);
+        stk.push(120);
+        System.out.println("Elements in Stack: " + stk);
+        result = stk.empty();
+        System.out.println("Is the stack empty?" + result);
+
+        //Stack peek() method
+        System.out.print("Top element: " + stk.peek());
+    }
+
+}
