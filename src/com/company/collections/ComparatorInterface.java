9a68b76c000201d0871fb78e5a4895c46e8dddbc
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-09-23 23:32:05 -0400

    comparator sort by name

--- src/com/company/collections/ComparatorInterface.java
+++ src/com/company/collections/ComparatorInterface.java
@@ -1,10 +1,10 @@
 package com.company.collections;
 
-import java.util.Comparator;
+import java.util.*;
 
 public class ComparatorInterface {
 
-    class Student{
+    static class Student{
         int id;
         String name;
         int age;
@@ -14,15 +14,58 @@ public class ComparatorInterface {
             this.name = name;
             this.age = age;
         }
-    }
 
-    class AgeComparator implements Comparator{
+
         @Override
-        public int compare(Object o1, Object o2){
-            return 0;
+        public String toString() {
+            return "Student{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
         }
     }
+
+    static class AgeComparator implements Comparator{
+        @Override
+        public int compare(Object o1, Object o2){
+            Student s1 = (Student)o1;
+            Student s2 = (Student)o2;
+
+            if(s1.age == s2.age){
+                return 0;
+            } else if (s1.age > s2.age){
+                return 1;
+            } else{
+                return -1;
+            }
+        }
+    }
+
+    static class NameComparator implements Comparator{
+        @Override
+        public int compare(Object o1, Object o2){
+            Student s1 = (Student)o1;
+            Student s2 = (Student)o2;
+
+            return s1.name.compareTo(s2.name);
+        }
+    }
+
     public static void main(String[] args){
+
+        ArrayList<Student> al = new ArrayList<>();
+        al.add(new Student(101,"Mark",27));
+        al.add(new Student(102,"Kid",37));
+        al.add(new Student(103,"Jai",30));
+
+        System.out.println("Sorting by Name");
+        Collections.sort(al, new NameComparator());
+        Iterator itr = al.iterator();
+        while(itr.hasNext()){
+            Student st = (Student)itr.next();
+            System.out.println(st.id+" "+st.name+" "+st.age+" ");
+        }
 
 
 
