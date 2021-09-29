5ad45a0658854adfdc033e1ff45d9f1cf50d5c1f
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-09-29 10:54:38 -0400

    try catch finally

---
+++ src/com/company/java/features/exceptions/JavaFinallyBlock.java
@@ -0,0 +1,18 @@
+package com.company.java.features.exceptions;
+
+public class JavaFinallyBlock {
+    public static void main(String[] args){
+
+
+        try{
+            int data = 25/0;
+            System.out.println(data);
+        }catch(ArithmeticException e){
+            System.out.println(e);
+        }finally {
+            System.out.println("finally block is always executed");
+        }
+
+        System.out.println("Rest of the code");
+    }
+}
---
+++ src/com/company/java/features/exceptions/JavaThrow.java
@@ -0,0 +1,5 @@
+package com.company.java.features.exceptions;
+
+public class JavaThrow {
+
+}
