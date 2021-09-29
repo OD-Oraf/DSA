ac9ff206728a355ec46985d9e555499d1e89cd52
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-09-29 10:19:23 -0400

    multiple try catch

--- src/com/company/java/features/exceptions/JavaTryCatch.java
+++ src/com/company/java/features/exceptions/JavaTryCatch.java
@@ -1,13 +1,24 @@
 package com.company.java.features.exceptions;
 
-public class JavaExceptions {
+public class JavaTryCatch {
     public static void main(String[] args){
         try{
             int data = 100/0;
         }catch (ArithmeticException e){
             System.out.println(e);
         }
-
         System.out.println("rest of the code");
+
+
+
+        try{
+            int data1 = 50/0;
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+        System.out.println("rest of the code");
+
     }
+
+
 }
