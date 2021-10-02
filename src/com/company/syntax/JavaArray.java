c9a73c01e8cfdde47f834897a37e3f45acde2a12
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-10-02 08:59:35 -0400

    java array syntax

---
+++ src/com/company/syntax/JavaArray.java
@@ -0,0 +1,21 @@
+package com.company.syntax;
+
+public class JavaArray {
+    public static void main(String[] args){
+
+        //Declaration
+        int[] arrOneDim = new int[10];
+        int[][] arrTwoDim = new int[10][20];
+        int[] arrLiteral = {1,2,3,4,5,56,66,7};
+        int[][] arrLiteralTwoDim = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+        //Access
+        for(int i = 0; i < arrLiteral.length; i++){
+            System.out.println(arrLiteral[i]);
+        }
+        System.out.println(" ");
+        for(int i : arrLiteral){
+            System.out.println(i);
+        }
+    }
+}
