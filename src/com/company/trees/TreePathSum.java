0bd2148c0cb44c68fdc573906da5ec7a96b6a54e
od <poseidonog@gmail.com>
2022-06-12 21:38:57 -0400

    add linked list cycle start

---
+++ src/com/company/trees/TreePathSum.java
@@ -0,0 +1,21 @@
+package com.company.trees;
+
+public class TreePathSum {
+
+    public static boolean hasPath(TreeNode root, int sum) {
+        if (root == null) {
+
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        System.out.println("Tree has path: " + hasPath(root, 23));
+        System.out.println("Tree has path: " + hasPath(root, 16));
+    }
+}
