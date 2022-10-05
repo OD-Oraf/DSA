0d968ea867294fba87e216a7772834597915c1f9
od <poseidonog@gmail.com>
2022-10-05 17:47:37 -0400

    remove print array method

--- src/com/company/algorithms/sorting/SelectionSort.java
+++ src/com/company/algorithms/sorting/SelectionSort.java
@@ -7,25 +7,17 @@ public class SelectionSort {
     public static void sort(int[] arr) {
         int n = arr.length;
         for(int i = 0; i < n-1; i++) { // for each index, find the next smallest value
-            int min_idx = i;
+            int minIndex = i;
             for (int j = i+1; j < n; j++) { // search for min value
-                if (arr[j] < arr[min_idx]) {
-                    min_idx = j;
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
-            int temp = arr[min_idx]; // swap current number and min number
-            arr[min_idx] = arr[i];
+            int temp = arr[minIndex]; // swap current number and min number
+            arr[minIndex] = arr[i];
             arr[i] = temp;
         }
     }
-
-//    public static void printArray(int[] arr) {
-//        int n = arr.length;
-//        for (int i = 0; i < n; ++i) {
-//            System.out.print(arr[i]+" ");
-//        }
-////        System.out.println();
-//    }
 
     public static void main(String[] args) {
         int arr[] = {64,25,12,22,11};
